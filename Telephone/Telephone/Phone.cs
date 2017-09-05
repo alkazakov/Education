3b3f51ef66ba171c4e1298b955136e0acb0132f5
Aleksandr Kazakov <aleksandr.kazakov@homag.com>
2017-09-05 16:07:10 +0200

    Builder

--- Telephone/Telephone/Phone.cs
+++ Telephone/Telephone/Phone.cs
@@ -2,6 +2,59 @@ using System;
 
 namespace Telephone
 {
+    interface ITelephoneBuilder
+    {
+        IDisplay BuildDisplay();
+        void BuildKeyboard();
+        void BuildOperatingSystem();
+        void BuildHardDrive();
+        void BuildMemory();
+        void BuildCamera();
+        void BuildWifiModule();
+        Phone Phone {get;}
+    }
+
+    class SamsungPhoneBuilder : ITelephoneBuilder
+    {
+        public IDisplay BuildDisplay()
+        {
+            DisplayFactory displayFactory = new DisplayFactory();
+            return displayFactory.Create<AmoledDisplay>();
+        }
+
+        public void BuildKeyboard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildOperatingSystem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildHardDrive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildMemory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildCamera()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BuildWifiModule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Phone Phone { get; }
+    }
+
     class Phone : ITurnOnOffable
     {
         private IDisplay _display;
